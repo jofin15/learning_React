4d75336556b46fb52a19277a3018aaacd2faddc5
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-09-02 05:54:56 +0000

    new

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,14 +1,17 @@
 
 import Chai from "./Chai";
-function App() {
 
+function App() {
+  const username="jofin johnosn"
   return (
     <>
-    <h1>hello jofin</h1>
+    <h1>hello {username}</h1>  
     <Chai />
-    
     </>
-  )
+  ) 
 }
 
 export default App
+
+
+// Note:- "{}" we call this as evaluated expression. you cannot write whole js script inside this . only the output of any function or call call variables
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp() {
+  return (
+    <div>
+      <h1>asdf</h1>
+    </div>
+  )
+}
+
+
+//react actually converts the above code in this object format
+const reactElement={
+  type:"a",
+  props:{
+      href:"https://google.com",
+      target:"_blank"
+  },
+  children:"click me to visit google"
+  }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+
   </React.StrictMode>,
 )
